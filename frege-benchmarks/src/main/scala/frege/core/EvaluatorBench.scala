fa4555634fb35079e02b0885565c236b2470757b
Ajay Viswanathan <ajay.viswanathan@gmail.com>
2021-09-26 17:13:04 +0700

    Adds single shot test

--- frege-benchmarks/src/main/scala/frege/core/EvaluatorBench.scala
+++ frege-benchmarks/src/main/scala/frege/core/EvaluatorBench.scala
@@ -9,16 +9,29 @@ import org.openjdk.jmh.annotations.{
 
 import java.util.concurrent.TimeUnit
 
-@BenchmarkMode(Array(Mode.Throughput))
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 class EvaluatorBench {
 
   @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  def standardEvaluatorSingle(state: EvaluatorState): Unit = {
+    state.requests.map(state.standardEvaluator.eval).foreach(println)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  def graphEvaluatorSingle(state: EvaluatorState): Unit = {
+    state.requests.map(state.graphEvaluator.eval).foreach(println)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime, Mode.Throughput))
   def standardEvaluator(state: EvaluatorState): Unit = {
     state.requests.map(state.standardEvaluator.eval)
   }
 
   @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime, Mode.Throughput))
   def graphEvaluator(state: EvaluatorState): Unit = {
     state.requests.map(state.graphEvaluator.eval)
   }
